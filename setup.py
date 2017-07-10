da84a69f31482e53d265f390e5c0becde468770f
Ian sun <lauer3912@gmail.com>
2017-07-10 15:22:34 +0800

    更新Setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='GmagonAPI',
       author_email='lauer3912@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.12.2', 'Flask_SQLAlchemy>=2.2',
-                        'Flask-RESTful>=0.3.6', 'SQLAlchemy-Utils>=0.32.14'
+                        'Flask-RESTful>=0.3.6', 'SQLAlchemy-Utils>=0.32.14',
                         'MySQL-python>=1.2.5', 'Flask-Login>=0.4.0'],
      )
