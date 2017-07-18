250dff41518d38c9795ac7d09a55ec98b43aa80d
Ian sun <lauer3912@gmail.com>
2017-07-18 20:33:46 +0800

    更新适应远程服务器

--- wsgi.py
+++ wsgi.py
@@ -426,7 +426,7 @@ def runApp():
 def oldRunApp():
     from wsgiref.simple_server import make_server
 
-    httpd = make_server('localhost', 8051, application)
+    # httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
     # httpd.handle_request()
 
