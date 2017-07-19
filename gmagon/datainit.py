5586bd5c923ebd89969fe34ecd3fddd983b0746b
Ian sun <lauer3912@gmail.com>
2017-07-19 10:20:12 +0800

    修改提交初始化数据入口

--- gmagon/datainit.py
+++ gmagon/datainit.py
@@ -7,6 +7,7 @@ from flask_restful import Resource, reqparse
 #: local
 from gmagon.common.model import retain as common_model_retain
 from gmagon.plugins.gif.data import init_all as subsystem_gif_data_create
+from gmagon.plugins.gif.util import constUriPrefix
 
 
 def init(api):
@@ -20,6 +21,6 @@ def init(api):
             }
 
     """
-    http://127.0.0.1:5000/gif/data_init
+    http://127.0.0.1:5000/plugin/gif/api/data_init
     """
-    api.add_resource(InitSubSystemGifData, '/gif/data_init')
+    api.add_resource(InitSubSystemGifData, constUriPrefix + '/data_init')
--- gmagon/plugins/gif/data.py
+++ gmagon/plugins/gif/data.py
@@ -233,12 +233,11 @@ def init_all():
     type_set_tag = __getSpecDataTypeItem(name='SetTag', description='Set标签的类型')
     type_user_analysis = __getSpecDataTypeItem(name='AnalysisType', description='分析数据类型')
 
-    db.session.commit()
-    return
-
-
     __initDataForItemAndSetCommon(type_item_category, type_item_tag)
     __initDataForItemAndSetCommon(type_set_category, type_set_tag)
+
+    db.session.commit()
+    return
 
     # 交叉使用import包，非全局引用，有效处理
     from api.gmagon.plugins.gif.test import init_test_data
